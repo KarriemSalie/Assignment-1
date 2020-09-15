eae1101f5969fdd6b05899972803e7cc13cfd1f3
KarriemSalie <69824526+KarriemSalie@users.noreply.github.com>
2020-09-15 14:46:11 +0200

    added final parts of code

---
+++ WIndowsFormsApp16/Character.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp16
+{
+ abstract class Character
+    {
+        protected int hP;
+        protected int max_HP;
+        protected int damage;
+        protected Tile[] char_vision;
+
+
+        public Character(int X, int Y, char symbol) : base(X, Y)
+        {
+
+        }
+        public int HP
+        {
+            get { return hP; }
+            set { hP = value; }
+        }
+
+        public int Max_HP
+        {
+            get { return max_HP; }
+            set { max_HP = value; }
+        }
+
+        public int Damage  
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+
+        public Tile[] Char_vision  
+        {
+            get { return char_vision; }
+            set { char_vision = value; }
+        }
+        public enum Movement  
+        {
+            No_movement,    
+            Up,             
+            Down,          
+            Left,
+            Right
+        }
+        public virtual void Attack(Character target)
+        {
+            target.HP = target.HP - this.Damage;
+        }
+        public bool IsDead()
+        {
+            if (this.HP >= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public virtual bool CheckRange(Character target)   
+        {
+            if (DistanceTo(target) <= 1)
+            {
+                return true;
+               
+            }
+            else
+            {
+                return false;
+               
+            }
+        }
+
+        private int DistanceTo(Character target)
+        {
+            int distance = (this.Xvalue - target.Xvalue) + (this.Yvalue - target.Yvalue);  
+          
+
+            int absoluteVal = Math.Abs(distance);    
+            return absoluteVal;
+        }
+
+        public void Move(Movement move)
+        {   
+            switch (move)
+            {
+                case Movement.No_movement:  
+                    this.Xvalue = this.Xvalue;
+                    this.Yvalue = this.Yvalue;
+                    break;
+                case Movement.Up:       
+                    this.Xvalue = this.Xvalue;
+                    this.Yvalue = this.Yvalue + 1;
+                    break;
+                case Movement.Down:    
+                    this.Xvalue = this.Xvalue;
+                    this.Yvalue = this.Yvalue - 1;
+                    break;
+                case Movement.Left:     
+                    this.Xvalue = this.Xvalue - 1;
+                    this.Yvalue = this.Yvalue;
+                    break;
+                case Movement.Right:   
+                    this.Xvalue = this.Xvalue + 1;
+                    this.Yvalue = this.Yvalue;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public abstract Movement ReturnMove(Movement move = 0);
+
+        public abstract override string ToString();
+    }
+
+
+
+}
---
+++ WIndowsFormsApp16/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp16
+{
+    abstract class Enemy :Character
+    {
+        protected Random randomobj;
+
+        public Random Randomobj
+        {
+            get { return randomobj; }
+            set { randomobj = value; }
+        }
+
+        public Enemy(int Xvalue, int Yvalue,int HP, int max_HP, int Enemy_damage, char symbol) : base(Xvalue, Yvalue, symbol)
+        {
+            this.HP = HP;
+            this.Max_HP = max_HP;
+            this.Damage = Enemy_Damage;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(this.GetType() + " at " + "[" + this.Xvalue + ", " + this.Yvalue + "]" + "(" + this.Damage + ")");
+        }
+    }
+
+    class Goblin : Enemy
+    {
+        public Goblin(int Xvalue, int Yvalue) : base(Xvalue, Yvalue,10, 10, 1, 'G')
+        {
+
+        }
+
+        public override Movement MoveReturn(Movement move)
+        {
+            switch (move)
+            {
+                case Movement.No_movement:
+                    return Movement.No_movement;
+
+                case Movement.Up:
+                    return Movement.Up;
+
+                case Movement.Down:
+                    return Movement.Down;
+
+                case Movement.Left:
+                    return Movement.Left;
+
+                case Movement.Right:
+                    return Movement.Right;
+
+                default:
+                    return Movement.No_movement;
+            }
+        }
+
+    }
+
+}
